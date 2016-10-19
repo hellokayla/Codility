ac66a0070ba9ce2d977201b131ed6b2d9332d023
Kayla N <niu@email.arizona.edu>
2016-10-18 19:12:33 -0500

    MaxCounter
    Fixed to 100/100

--- MaxCounter_Problem.java
+++ MaxCounter_Problem.java
@@ -5,25 +5,35 @@ class Solution {
     public int[] solution(int N, int[] A) {
         // counter can only hold up to N 
         int[] counter = new int[N];
-        int maxvalue = 0; 
+        int maxvalue = 0;
+        int maxcounter = 0;
         int position = 0;
         for (int i = 0; i < A.length; i++) {
             
             if (A[i] <= N && A[i] >=1) {
-             
-                // Want to increase X
+                if (counter[A[i]-1] < maxcounter)
+                    counter[A[i]-1] = maxcounter;
+
                 position = A[i]-1;
                 counter[position]++;
                 // Need a way to store the maxcounter
                 if (counter[position]>maxvalue) {   
                  maxvalue = counter[position];  
-                 }
+                }
                  
             }
             if (A[i] == (N+1)) {
                 
-               Arrays.fill(counter,maxvalue);
+               maxcounter = maxvalue; 
             }       
+        }
+        
+        for (int j = 0; j<N; j++) {
+         
+         if (counter[j] < maxcounter) {
+             counter[j] = maxcounter; 
+            }
+            
         }
         
         return counter; 
